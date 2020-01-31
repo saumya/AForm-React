2b0210dbf863450859d124004231144b6d13fbc2
saumya <2saumyaray@gmail.com>
2020-01-31 14:34:03 +0530

    WIP: new demo component added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ import Scheduler1 from './components/scheduler1.comp';
 function App() {
   return (
     <div className="App">
+
+    	<div className="container">
+    		<div className="columns is-mobile is-centered">
+    			<div className="column is-half">
+    				<h1 className="title"> Demo </h1>
+    				<h2 className="subtitle"> Experience the applications. </h2>
+    				<div className="buttons">
+    					<button className="button is-dark"> Patient </button>
+    					<button className="button is-dark"> Doctor </button>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+	    
       <Scheduler1 name="Demo | " msg='Date & Doctor'/>
     </div>
   );
